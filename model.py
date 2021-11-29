b4378a0c90e2fe7dcc3006d4e5a17bcb7854a441
attrung <ngtrung2901@gmail.com>
2021-11-29 17:54:43 +0000

    training code n stuff

--- model.py
+++ model.py
@@ -5,19 +5,42 @@ import torch.optim as optim
 
 class LSTMClassifier(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, target_size):
+    def __init__(self, input_dim, hidden_dim, target_size, device = "cpu"):
         super(LSTMClassifier, self).__init__()
         self.hidden_dim = hidden_dim
+        self.device = device
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(input_dim, hidden_dim)
+        self.conv = nn.Sequential(
+            nn.Conv1d(input_dim, 64, 3),
+            nn.MaxPool1d(2),
+            nn.Dropout(p=0.05, inplace = False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(64, 128, 5),
+            nn.MaxPool1d(2),
+            nn.Dropout(p=0.05, inplace = False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 256, 5),
+            nn.MaxPool1d(2),
+            nn.ReLU(inplace = True),
+            nn.Conv1d(256, 256, 7),
+            nn.MaxPool1d(3),
+            nn.ReLU(inplace = True),
+        )
+
+        self.lstm = nn.LSTM(256, hidden_dim)
         self.linear = nn.Linear(hidden_dim, target_size)
+        self.softmax = nn.Softmax(dim = 1)
 
     def forward(self, data):
-        hidden = (torch.randn(1, 1, self.hidden_dim), torch.randn(1, 1, self.hidden_dim))
-        for i in data:
-            out, hidden = self.lstm(i.view(1, 1, -1), hidden)
-        pred = self.linear(out)
-        pred = F.log_softmax(pred, dim=1)
+        hidden = (
+            torch.randn(1, 1, self.hidden_dim).to(self.device), 
+            torch.randn(1, 1, self.hidden_dim).to(self.device),
+        )
+        data = data.view(1, data.size(2), data.size(1))
+        out = self.conv(data)
+        out = out.view(out.size(-1), 1, 256)
+        out, hidden = self.lstm(out, hidden)
+        pred = self.linear(out[-1])
         return pred
--- train.py
+++ train.py
@@ -7,18 +7,43 @@ from model import LSTMClassifier
 
 torch.manual_seed(1)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 train_data = MovementDataset("train")
 valid_data = MovementDataset("valid")
 
-model = LSTMClassifier(19, 64, train_data.label_size)
-loss_function = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=True, num_workers= 1)
+valid_data = torch.utils.data.DataLoader(valid_data, batch_size=1, shuffle=True, num_workers= 1)
 
-for epoch in range(1):
-    for data, target in train_data:
+model = LSTMClassifier(19, 128, train_data.label_size, device)
+model = model.to(device)
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.03)
+
+total_loss = 0
+idx = 0
+acc = 0
+for step in range(100000):
+    model.train()
+    for data, target in train_loader:
+        optimizer.zero_grad()
         model.zero_grad()
+        data = data.to(device)
+        target = target.to(device)
         output = model(data)
+        _, predicted = torch.max(output.data, 1)
+        if predicted == target:
+            acc += 1
         loss = loss_function(output, target)
         loss.backward()
         optimizer.step()
-        print(loss)
+        total_loss += loss
+        idx += 1
+        if idx == 1000:
+            print(total_loss/1000)
+            print(acc / 1000)
+            total_loss = 0
+            idx = 0
+            acc = 0
+        
+    
